f10033b8388e3cf1e4bbd148e1ac37ec98e5d134
Manuela Viola <manuela.viola@rootstrap.com>
2021-03-10 19:21:20 -0300

    feat: add rating

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -3,10 +3,21 @@ import React from "react";
 import { getImagePath } from "utils/helpers";
 import "./MovieItem.css";
 
-const MovieItem = ({ movie: { originalTitle, posterPath } = {} }) => (
+const MovieItem = ({ movie: { title, posterPath, voteAverage } = {} }) => (
   <div className="movie-item-container">
-    <img alt="poster" className="poster-image" src={getImagePath(posterPath)} />
-    <p className="title">{originalTitle}</p>
+    <div className="image-badge-container">
+      <img
+        alt="poster"
+        className="poster-image"
+        src={getImagePath(posterPath)}
+      />
+      <div className="badge">
+        <span className="rate">
+          <h6>{voteAverage}</h6>
+        </span>
+      </div>
+    </div>
+    <p className="title">{title}</p>
   </div>
 );
 
